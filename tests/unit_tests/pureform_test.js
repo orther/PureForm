f45d6cd7e204ac109d12bea5bf4a9421fa4ddf04
Brandon Orther <brandon.orther@gmail.com>
2011-01-21 13:52:32 -0800

    deleted moved files

--- tests/unit_tests/pureform_test.js
+++
@@ -1,8 +0,0 @@
-module('PureForm');
-test('validator', function () {
-   equals(pureform().registerValidator("validator_1", (function () {})), undefined, 'registerValidator()');
-   ok(pureform().isValidatorRegistered("validator_1"), 'isValidatorRegistered()');
-   equals(pureform().unregisterValidator("validator_1"), undefined, 'unregisterValidator()');
-   ok(!pureform().isValidatorRegistered("validator_1"), 'isValidatorRegistered()');
-});
-

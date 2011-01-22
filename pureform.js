5b8366f3f6eaffe9b43bf4e5ad7998c1977cd57b
Brandon Orther <brandon.orther@gmail.com>
2011-01-21 17:54:24 -0800

    got addField and addFields working

--- pureform.js
+++ pureform.js
@@ -1,7 +1,110 @@
 var pureForm = (function () {
 
-    var
-        _forms = {};
+    var _forms = {};
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * The form object all created forms are based created from.
+     */
+    function formObject () {
+
+        this._aggregates  = {};
+        this._fields      = {};
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        /**
+         * Add aggregate to form.
+         *
+         * @param name   (string)
+         * @param params (object)
+         *
+         * @return (object) Returns this object to allow chaining.
+         */
+        this.addAggregate = function (name, params) {
+
+            if (typeof name != "string")
+                throw "pureForm::addAggregate >> `name` not a string";
+
+            if (name in this._aggregates)
+                throw "pureForm::addAggregate >> Aggregate with name `" + name + "` already exists";
+
+            if (typeof params != "object")
+                throw "pureForm::addAggregate >> `params` not an object";
+
+            // add field
+            this._aggregates[name] = params;
+
+            return this;
+
+        };
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        /**
+         * Add multiple aggregates to form.
+         *
+         * @param aggregates (object)
+         *
+         * @return (object) Returns this object to allow chaining.
+         */
+        this.addAggregates = function (aggregates) {
+
+            for (aggregate_name in aggregates)
+                this.addAggregate(aggregate_name, aggregates[aggregate_name]);
+
+            return this;
+
+        };
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        /**
+         * Add field to form.
+         *
+         * @param name   (string)
+         * @param params (object)
+         *
+         * @return (object) Returns this object to allow chaining.
+         */
+        this.addField = function (name, params) {
+
+            if (typeof name != "string")
+                throw "pureForm::addField >> `name` not a string";
+
+            if (name in this._fields)
+                throw "pureForm::addField >> Field with name `" + name + "` already exists";
+
+            if (typeof params != "object")
+                throw "pureForm::addField >> `params` not an object";
+
+            // add field
+            this._fields[name] = params;
+
+            return this;
+
+        };
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        /**
+         * Add multiple fields to form.
+         *
+         * @param fields (object)
+         *
+         * @return (object) Returns this object to allow chaining.
+         */
+        this.addFields = function (fields) {
+
+            for (field_name in fields)
+                this.addField(field_name, fields[field_name]);
+
+            return this;
+
+        };
+
+    };
 
     // -----------------------------------------------------------------------------------------------------------------
 
@@ -17,7 +120,7 @@ var pureForm = (function () {
         if (name in _forms)
             throw "PureForm::create() >> Failed, form named `" + name + "` already exist.";
 
-        _forms[name] = {};
+        _forms[name] = new formObject;
 
         return _forms[name];
 

d73662703e20233088d1ac7f65f4298affaaa941
Brandon Orther <brandon.orther@gmail.com>
2011-02-24 23:30:14 -0800

    added a unit test for adding a field with min/max validation

--- test/unit/pureform_test.js
+++ test/unit/pureform_test.js
@@ -12,3 +12,15 @@ PureFormTestCase.prototype.testAddField = function () {
     /*:DOC += <input id="first_name" value="Brandon" /> */
     assertObject(pureForm().getForm("test_form").addField("first_name", {type: "string", name: "First Name"}));
 };
+
+PureFormTestCase.prototype.testAddFieldValidator = function () {
+    /*:DOC += <input id="last_name" value="Orther" /> */
+    assertObject(pureForm().getForm("test_form").addField("last_name", {
+        type: "string",
+        name: "Last Name",
+        validators: {
+            "min": {"min": 3, "error": "%field_name% must be at least %min% characters long."},
+            "max": {"max": 30, "error": "%field_name% must be less than %max% characters long."}
+        }
+    }));
+};
